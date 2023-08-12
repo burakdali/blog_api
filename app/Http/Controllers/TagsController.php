aaff2ad237cf86f4a43ab9602a813b397acfebbd
burakdali <burakdali@yahoo.com>
2023-08-12 14:17:19 +0300

    feat: added model, controller, seeder, factory and migration files. the addition done whithout changing files content.

---
+++ app/Http/Controllers/TagsController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tags;
+use Illuminate\Http\Request;
+
+class TagsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Tags  $tags
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tags $tags)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Tags  $tags
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tags $tags)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Tags  $tags
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tags $tags)
+    {
+        //
+    }
+}
