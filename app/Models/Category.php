1adcd8f8d44da49ce5a014b09a571eb62e76f969
burakdali <burakdali@yahoo.com>
2023-08-14 02:16:22 +0300

    feat: added pivot category_tag table. feat: added relationship. feat: added requests for Models.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
@@ -12,5 +13,9 @@ class Category extends Model
     function posts(): HasMany
     {
         return $this->hasMany(Post::class);
+    }
+    function tag(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
     }
 }
---
+++ database/migrations/2023_08_13_221651_create_category_tag_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_tag', function (Blueprint $table) {
+            $table->foreignId('category_id');
+            $table->foreign('category_id')->on('categories')->references('id')->cascadeOnDelete();
+            $table->foreignId('tag_id');
+            $table->foreign('tag_id')->on('tags')->references('id')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_tag');
+    }
+};
